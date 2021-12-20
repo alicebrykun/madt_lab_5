56842ee8e970006da2ffefdfa8de5efd73e52cd9
alicebrykun <fkbcfm@gmail.com>
2021-12-20 20:54:05 +0200

    initial commit

---
+++ app/src/main/java/com/example/madt_lab_5/Const.java
@@ -0,0 +1,7 @@
+package com.example.madt_lab_5;
+
+public class Const {
+    private Const() { }
+    public static final String CUBE_NODE = "Cube";
+    public static final String ECB_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+}
